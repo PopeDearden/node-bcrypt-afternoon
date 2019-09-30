3d88a7992118db6990bc1e5c15bb43d892b77b14
taylor dearden <taylordearden@gmail.com>
2019-09-29 19:57:53 -0600

    finished it

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
+const treasureCtrl =require ('./controllers/treasureController')
 const express = require('express');
 const session = require('express-session')
 const massive = require('massive')
 const authCtrl = require('./controllers/authController')
-
+const auth = require('./middleware/authMiddleware');
 
 
 const { SESSION_SECRET, CONNECTION_STRING, PORT } = process.env
@@ -25,5 +26,11 @@ app.use(session({
 
 app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
+app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get('/api/treasure/all', auth.usersOnly, treasureCtrl.getAllTreasure);
 
 app.listen(PORT, ()=>console.log(`Cows making ${PORT} pies`))
